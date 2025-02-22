30d05ecee20a9ba51211dc3f792757a3afaf7e17
Vladimir Zaets <vzaets98@mail.ru>
2025-02-22 13:15:50 +0700

    Function to start Chrome with necessary language was added to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,3 +1,16 @@
+from selenium import webdriver
+import pytest
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help='Choose language')
+
+@pytest.fixture(scope='function')
+def browser(request):
+    language = request.cofig.getoption('language')
+    link = f'https://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/'
+    browser = webdriver.Chrome()
+    browser.get(link)
+    yield browser
+    print('\nquit browser')
+    browser.quit()
